d71d44fc47d00eb60fe108a770343a707a8f0129
Reeva Gupta <114238405+reevacodes@users.noreply.github.com>
2025-07-22 23:20:00 +0530

    Add files via upload

---
+++ tail_deletion.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* convertArrToLL(vector<int>&arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp =new Node(arr[i]);
+        mover->next = temp;
+        mover=temp;     //mover=mover->next
+    }
+    return head;
+}
+
+Node* deleteTail(Node* head){
+    if(head==NULL || head->next==NULL) return NULL;
+    Node* temp = head;
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }
+    free(temp->next);
+    temp->next = nullptr;
+    return head;
+}
+
+void print(Node* head){
+    while(head){
+        cout<<head->data<<" ";
+        head = head->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    vector<int> arr = {2,5,8,7};
+    Node* head = convertArrToLL(arr);
+    print(head);
+    deleteTail(head);
+    print(head);
+    return 0;
+} 
